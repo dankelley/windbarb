242e04cd10e726a60f944496d973da8b2a630aeb
dankelley <kelley.dan@gmail.com>
2024-03-19 15:42:19 -0300

    use 'length' instead of 'scale'

--- wb.R
+++ wb.R
@@ -19,9 +19,9 @@ feathers <- function(u, step = 10, debug = FALSE) {
 }
 
 windBarbs <- function(longitude, latitude, u, v,
-                      scale = 1, step = 10, lwd = 1, col = 1,
+                      length = 5, step = 10, lwd = 1, col = 1,
                       round10 = FALSE, debug = FALSE) {
-    scaleXY <- scale * 111e3 # per metre
+    lengthXY <- length * 111e3 # per metre
     lon0 <- longitude
     lat0 <- latitude
     theta <- 180 / pi * atan2(v, u)
@@ -32,8 +32,8 @@ windBarbs <- function(longitude, latitude, u, v,
     }
     S <- sinpi(theta / 180)
     C <- cospi(theta / 180)
-    lat1 <- lat0 + C * scale
-    lon1 <- lon0 + S * scale / cospi(lat0 / 180)
+    lat1 <- lat0 + C * length
+    lon1 <- lon0 + S * length / cospi(lat0 / 180)
     xy0 <- lonlat2map(as.vector(lon0), as.vector(lat0))
     x0 <- xy0$x
     y0 <- xy0$y
@@ -46,7 +46,7 @@ windBarbs <- function(longitude, latitude, u, v,
     barbLwd <- lwd
     barbCol <- col
     flagCol <- col
-    barbLength <- 0.2 * scaleXY
+    barbLength <- 0.2 * lengthXY
     # D <- 0.11 # barb separation (Used??)
     triangleWidth <- 2 * barbLength * cospi(phi / 180)
     triangleLength <- 0.5 * triangleWidth / cospi(phi / 180) # or sin?
@@ -54,8 +54,8 @@ windBarbs <- function(longitude, latitude, u, v,
     ds <- 0.2 * cospi(phi / 180)
     if (debug) {
         cat(sprintf(
-            "scale: %.4f, barbLength: %.4f, barbSpace: %.4f, triangleWidth: %.4f\ntriangleLength: %.4f\n",
-            scale, barbLength, barbSpace, triangleWidth, triangleLength
+            "length: %.4f, barbLength: %.4f, barbSpace: %.4f, triangleWidth: %.4f\ntriangleLength: %.4f\n",
+            length, barbLength, barbSpace, triangleWidth, triangleLength
         ))
     }
     knots <- TRUE
@@ -69,8 +69,8 @@ windBarbs <- function(longitude, latitude, u, v,
     speed <- 5L * as.integer(round(speed / 5))
     Spage <- sinpi(thetaPage / 180)
     Cpage <- cospi(thetaPage / 180)
-    x1 <- x0 + scaleXY * Cpage
-    y1 <- y0 + scaleXY * Spage
+    x1 <- x0 + lengthXY * Cpage
+    y1 <- y0 + lengthXY * Spage
     notStill <- speed != 0
     segments(x0[notStill], y0[notStill], x1[notStill], y1[notStill], col = barbCol, lwd = barbLwd)
     f <- feathers(speed, step = step, debug = debug)
@@ -145,8 +145,8 @@ windBarbs <- function(longitude, latitude, u, v,
                     if (debug) {
                         cat(
                             sprintf(
-                                "delta=%.3f s=%.3f thetaPage=%.3f scale=%.3f triangleWidth=%.4f triangleLength=%.4f\n",
-                                delta, s, thetaPage, scale, triangleWidth, triangleLength
+                                "delta=%.3f s=%.3f thetaPage=%.3f length=%.3f triangleWidth=%.4f triangleLength=%.4f\n",
+                                delta, s, thetaPage, length, triangleWidth, triangleLength
                             )
                         )
                     }
@@ -154,7 +154,7 @@ windBarbs <- function(longitude, latitude, u, v,
                     s <- s - 3 * ds # (triangleWidth + barbSpace)
                 } else if (thisCode == 2 || thisCode == 3) { # barb
                     if (lowestNonzero) {
-                        s <- s - ds # barbSpace / scale
+                        s <- s - ds # barbSpace / length
                     }
                     x <- x0i + s * (x1i - x0i)
                     y <- y0i + s * (y1i - y0i)
@@ -169,12 +169,10 @@ windBarbs <- function(longitude, latitude, u, v,
 
 # FIXME: if put into oce, document parameters, explain
 # plot, give an example, set up familial links with
-# sibling functions, etc. Possibly 'scale' should be
-# called 'length', now that I look at the parameters
-# with oce in mind.
+# sibling functions, etc.
 mapDirectionFieldBarbs <- function(
     longitude, latitude, u, v,
-    scale = 1, step = 10, col = par("fg"), lwd = par("lwd"),
+    length = 1, step = 10, col = par("fg"), lwd = par("lwd"),
     debug = FALSE, ...) {
     if (is.matrix(u)) {
         if (is.vector(longitude) && is.vector(latitude)) {
@@ -188,6 +186,6 @@ mapDirectionFieldBarbs <- function(
     latitude <- oce:::angleShift(latitude)
     windBarbs(longitude, latitude,
         u = as.vector(u), v = as.vector(v),
-        scale = scale, step = step, debug = debug, col = col, lwd = lwd
+        length = length, step = step, debug = debug, col = col, lwd = lwd
     )
 }
